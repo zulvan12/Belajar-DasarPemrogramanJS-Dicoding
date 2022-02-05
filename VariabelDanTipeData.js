a97a11b42c021bb5a0108997e5b29b0dbe6b01c4
zulvanF <zulvanfahmi1612@gmail.com>
2022-02-05 12:18:29 +0700

    Jawaban kuis coding

---
+++ VariabelDanTipeData.js
@@ -0,0 +1,14 @@
+/**
+ * TODO:
+ * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+ *  - firstName: bertipe data string, dengan nilai nama depan Anda.
+ *  - lastName: bertipe data string, dengan nilai nama belakang Anda.
+ *  - age: bertipe data number, dengan nilai umur Anda.
+ *  - isMarried: bertipe data boolean, dengan bebas Anda tentukan.
+ */
+
+// TODO
+const firstName = "Sumanto";
+const lastName = "Uchiha";
+let age = 25;
+let isMarried = false;
